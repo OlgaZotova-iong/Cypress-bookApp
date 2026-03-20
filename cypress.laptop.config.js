71e3a8f32a524762144092d90d9d869f08e23865
OlgaZotova-iong <olgazotova197602@gmail.com>
2026-03-20 18:40:02 +0300

    Solving the second task

--- cypress.laptop.config.js
+++ cypress.laptop.config.js
@@ -1,20 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  retries: 2,
   e2e: {
     baseUrl: "http://localhost:3000",
     viewportWidth: 1366,
     viewportHeight: 768,
+    retries: 2,
     supportFile: "cypress/support/e2e.js",
     setupNodeEvents(on, config) {
       //
     },
   },
 });
-
-
-
-
-
 
